0859e60d34475e4e4ecb232b6d19609e176b3e5d
CreepyPvP <interruptedThread@gmail.com>
2022-07-18 22:46:08 +0200

    Some changes

---
+++ NeuralWaveFunctionCollapse/MachineLearning/Classifier.cs
@@ -0,0 +1,12 @@
+﻿using NeuralWaveFunctionCollapse.Math;
+
+namespace NeuralWaveFunctionCollapse.MachineLearning;
+
+public interface IClassifier
+{
+
+    int Classify(Tensor input);
+
+    void Train(Tensor input, DataContainer<int> labels);
+
+}
---
+++ NeuralWaveFunctionCollapse/WaveFunctionCollapse/Models/ClassifierModel.cs
@@ -0,0 +1,43 @@
+﻿using NeuralWaveFunctionCollapse.MachineLearning;
+using NeuralWaveFunctionCollapse.Math;
+
+namespace NeuralWaveFunctionCollapse.WaveFunctionCollapse.Models;
+
+public class ClassifierModel: IWaveFunctionModel
+{
+
+
+    private readonly int _radius;
+    
+    private IClassifier _classifier;
+
+    
+    public ClassifierModel(IClassifier classifier, int radius)
+    {
+        _classifier = classifier;
+        _radius = radius;
+    }
+
+    public void Build()
+    {
+        var trainingSize = 0;
+        var trainingData = new Tensor(Shape.Of(trainingSize, GetKernelSize(_radius),));
+    }
+    
+    public bool Impacts(int collapseX, int collapseY, int posX, int posY)
+    {
+        return System.Math.Abs(collapseX - posX) <= 1 && System.Math.Abs(collapseY - posY) <= 1;
+    }
+
+    public Tensor CalculateDistribution(int x, int y, DataContainer<int> collapsed, Tensor additionalData)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private int GetKernelSize(int radius)
+    {
+        return (2 * radius + 1) * (2 * radius + 1) - 1;
+    }
+    
+}
